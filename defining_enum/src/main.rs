a18ce03180da5652baa87f9ca88dd74c5de86a52
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-12-27 11:54:23 -0300

    Adicionando enum do tipo Option<T> e controle com match

---
+++ defining_enum/src/main.rs
@@ -0,0 +1,27 @@
+enum IpAddrKind<T> {
+    V4(T),
+    V6(T),
+    None,
+}
+
+fn ip_type<T>(ip: IpAddrKind<T>) {
+    match ip{
+        IpAddrKind::V4(_t) => {
+            println!("ip versao 4");
+        },
+        IpAddrKind::V6(_t) => {
+            println!("ip versao 6");
+        },
+        IpAddrKind::None=> {println!("None found")},
+    }
+}
+
+fn main() {
+    let four = IpAddrKind::V4(String::from("127.0.0.1"));
+    let six = IpAddrKind::V6(String::from("::1"));
+    let none: IpAddrKind<i32> = IpAddrKind::None;
+    ip_type(four);
+    ip_type(six);
+    ip_type(none);
+    
+}
