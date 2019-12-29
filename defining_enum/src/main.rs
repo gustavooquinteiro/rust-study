435189201b33702b693970030f1a30361562bc70
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-12-29 08:54:53 -0300

    Adicionando exemplo de uso do if let

--- defining_enum/src/main.rs
+++ defining_enum/src/main.rs
@@ -4,15 +4,21 @@ enum IpAddrKind<T> {
     None,
 }
 
-fn ip_type<T>(ip: IpAddrKind<T>) {
-    match ip{
-        IpAddrKind::V4(_t) => {
-            println!("ip versao 4");
-        },
-        IpAddrKind::V6(_t) => {
-            println!("ip versao 6");
-        },
-        IpAddrKind::None=> {println!("None found")},
+fn ip_type<T>(ip: &IpAddrKind<T>) {
+    let _localhost = String::from("127.0.0.1");
+    if let IpAddrKind::V4(_localhost) = ip {
+        println!("Localhost identificado");
+    } else{
+        println!("Não é localhost");
+        match ip{
+            IpAddrKind::V4(_t) => {
+                println!("ip versao 4");
+            },
+            IpAddrKind::V6(_t) => {
+                println!("ip versao 6");
+            },
+            IpAddrKind::None=> {println!("None found")},
+        }
     }
 }
 
@@ -20,8 +26,8 @@ fn main() {
     let four = IpAddrKind::V4(String::from("127.0.0.1"));
     let six = IpAddrKind::V6(String::from("::1"));
     let none: IpAddrKind<i32> = IpAddrKind::None;
-    ip_type(four);
-    ip_type(six);
-    ip_type(none);
     
+    ip_type(&four);
+    ip_type(&six);
+    ip_type(&none);    
 }
