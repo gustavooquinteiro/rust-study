342c62413a45255a8a76d71a0a391ddf807fb0c7
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-02 11:32:54 -0300

    Formatando código com rustfmt

--- sorting_vector/src/main.rs
+++ sorting_vector/src/main.rs
@@ -5,17 +5,16 @@ fn main() {
     let mut n = String::new();
     io::stdin().read_line(&mut n).expect("Expect string");
     let n: usize = n.trim().parse().expect("Expect number");
-    
+
     let mut vec: Vec<u32> = Vec::new();
     for _ in 0..n {
         let mut number = String::new();
         io::stdin().read_line(&mut number).expect("Expect string");
         let number: u32 = number.trim().parse().expect("Expect number");
-        
+
         vec.push(number);
     }
-    
-    sorting_vector::sorting::quicksort::quicksort(&mut vec, &|x, y| x < y);
+
+    sorting_vector::sorting::mergesort::mergesort(&mut vec, &|x, y| x < y);
     println!("{:?}", vec);
-    
 }
